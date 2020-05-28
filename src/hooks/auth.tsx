a6c5ebded660182e8fc2299926e85f41b507701c
Renan Bertoldo <renan.rbertoldo@gmail.com>
2020-05-28 00:04:18 -0300

    feat: Added updateUser to update profile image

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -20,6 +20,7 @@ interface AuthContextData {
   user: User;
   signIn(credentials: SignInCreadentials): Promise<void>;
   signOut(): void;
+  updateUser(user: User): void;
 }
 
 export const AuthContext = createContext<AuthContextData>(
@@ -60,8 +61,22 @@ export const AuthProvider: React.FC = ({ children }) => {
     setData({} as AuthState);
   }, []);
 
+  const updateUser = useCallback(
+    (user: User) => {
+      localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+
+      setData({
+        token: data.token,
+        user,
+      });
+    },
+    [setData, data.token],
+  );
+
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+    <AuthContext.Provider
+      value={{ user: data.user, signIn, signOut, updateUser }}
+    >
       {children}
     </AuthContext.Provider>
   );
